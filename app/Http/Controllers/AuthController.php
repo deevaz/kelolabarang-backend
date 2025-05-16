ce6c3b457adb5db42a2922b08a50aac6bf1850fa
deevaz <abdaziz1713@gmail.com>
2025-05-16 09:50:13 +0700

    fix: perbaikan bug edit profile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -89,7 +89,7 @@ class AuthController extends Controller
             'username'        => 'sometimes|required|string|max:255|unique:users,username,' . $user->id,
             'email'           => 'sometimes|required|string|email|max:255|unique:users,email,' . $user->id,
             'profile_picture' => 'sometimes|nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'password'        => 'sometimes|required|string|min:6',
+            // 'password'        => 'sometimes|required|string|min:6',
         ]);
 
         if ($request->has('name')) {
